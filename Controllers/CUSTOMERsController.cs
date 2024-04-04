b629d2cc7b0a996f5ba3ebaf236fdd886b3a3a22
WeenieMelon <manuelsurkc@gmail.com>
2024-04-03 20:07:03 -0500

    Add project files.

---
+++ Controllers/CUSTOMERsController.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Group12_iCLOTHINGAPP.Models;
+
+namespace Group12_iCLOTHINGAPP.Controllers
+{
+    public class CUSTOMERsController : Controller
+    {
+        private Group12_iCLOTHINGDBEntities2 db = new Group12_iCLOTHINGDBEntities2();
+
+        // GET: CUSTOMERs
+        public ActionResult Index()
+        {
+            var cUSTOMER = db.CUSTOMER.Include(c => c.USERPASSWORD);
+            return View(cUSTOMER.ToList());
+        }
+
+        // GET: CUSTOMERs/Details/5
+        public ActionResult Details(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            CUSTOMER cUSTOMER = db.CUSTOMER.Find(id);
+            if (cUSTOMER == null)
+            {
+                return HttpNotFound();
+            }
+            return View(cUSTOMER);
+        }
+
+        // GET: CUSTOMERs/Create
+        public ActionResult Create()
+        {
+            ViewBag.CUSTOMERID = new SelectList(db.USERPASSWORD, "USERID", "ACCNAME");
+            return View();
+        }
+
+        // POST: CUSTOMERs/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "CUSTOMERID,NAME,SHIPPINGADDRESS,BILLINGADDRESS,DOB,GENDER")] CUSTOMER cUSTOMER)
+        {
+            if (ModelState.IsValid)
+            {
+                db.CUSTOMER.Add(cUSTOMER);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.CUSTOMERID = new SelectList(db.USERPASSWORD, "USERID", "ACCNAME", cUSTOMER.CUSTOMERID);
+            return View(cUSTOMER);
+        }
+
+        // GET: CUSTOMERs/Edit/5
+        public ActionResult Edit(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            CUSTOMER cUSTOMER = db.CUSTOMER.Find(id);
+            if (cUSTOMER == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.CUSTOMERID = new SelectList(db.USERPASSWORD, "USERID", "ACCNAME", cUSTOMER.CUSTOMERID);
+            return View(cUSTOMER);
+        }
+
+        // POST: CUSTOMERs/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "CUSTOMERID,NAME,SHIPPINGADDRESS,BILLINGADDRESS,DOB,GENDER")] CUSTOMER cUSTOMER)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(cUSTOMER).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.CUSTOMERID = new SelectList(db.USERPASSWORD, "USERID", "ACCNAME", cUSTOMER.CUSTOMERID);
+            return View(cUSTOMER);
+        }
+
+        // GET: CUSTOMERs/Delete/5
+        public ActionResult Delete(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            CUSTOMER cUSTOMER = db.CUSTOMER.Find(id);
+            if (cUSTOMER == null)
+            {
+                return HttpNotFound();
+            }
+            return View(cUSTOMER);
+        }
+
+        // POST: CUSTOMERs/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(string id)
+        {
+            CUSTOMER cUSTOMER = db.CUSTOMER.Find(id);
+            db.CUSTOMER.Remove(cUSTOMER);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Group12_iCLOTHINGAPP.MvcApplication" Language="C#" %>
