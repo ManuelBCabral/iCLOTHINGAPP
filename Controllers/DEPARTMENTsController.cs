4f4e07f73cbbad264564cb4322444caa15579084
WeenieMelon <manuelsurkc@gmail.com>
2024-04-08 14:45:09 -0500

    View Products working

--- Controllers/DEPARTMENTsController.cs
+++ Controllers/DEPARTMENTsController.cs
@@ -134,13 +134,20 @@ namespace Group12_iCLOTHINGAPP.Controllers
             {
                 var categories = db.CATEGORY.Where(c => c.DEPID == DepdepId).ToList();
                 return View(categories);
-                //var ViewModel = new CATEGORY();
-                //ViewModel.CATID = db.CATEGORY.Include(x => x.DEPID == objUser.DEPID).ToList();
+            }
+
+
+        }
+        public ActionResult FilteredCustomerViewPro(string CatId)
+        {
+            using (Group12_iCLOTHINGDBEntities2 db = new Group12_iCLOTHINGDBEntities2())
+            {
+                var products = db.PRODUCT.Where(c => c.CATID == CatId).ToList();
+                return View(products);
             }
 
 
         }
     }
-    
-
 }
+    
